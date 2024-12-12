5c8313f397548e1e93479fb7a5415722b63af73b
Andrew Hinh <ajhinh@gmail.com>
2024-12-11 23:53:18 -0800

    draft of gran delete

--- frontend/app.py
+++ frontend/app.py
@@ -216,23 +216,6 @@ def modal_get():  # noqa: C901
 
     # ui
     ## components
-    def icon(
-        svg,
-        width="35",
-        height="35",
-        viewBox="0 0 15 15",
-        fill="none",
-        cls="rounded p-0.5 hover:bg-zinc-700 cursor-pointer",
-    ):
-        return fh.Svg(
-            fh.NotStr(svg),
-            width=width,
-            height=height,
-            viewBox=viewBox,
-            fill=fill,
-            cls=cls,
-        )
-
     def gen_view(
         g: GenRead,
         session,
@@ -366,6 +349,30 @@ def modal_get():  # noqa: C901
 
             return (
                 fh.Div(
+                    fh.Button(
+                        fh.Svg(
+                            """<path d="M10 12L14 16M14 12L10 16M18 6L17.1991 18.0129C17.129 19.065 17.0939 19.5911 16.8667 19.99C16.6666 20.3412 16.3648 20.6235 16.0011 20.7998C15.588 21 15.0607 21 14.0062 21H9.99377C8.93927 21 8.41202 21 7.99889 20.7998C7.63517 20.6235 7.33339 20.3412 7.13332 19.99C6.90607 19.5911 6.871 19.065 6.80086 18.0129L6 6M4 6H20M16 6L15.7294 5.18807C15.4671 4.40125 15.3359 4.00784 15.0927 3.71698C14.8779 3.46013 14.6021 3.26132 14.2905 3.13878C13.9376 3 13.523 3 12.6936 3H11.3064C10.477 3 10.0624 3 9.70951 3.13878C9.39792 3.26132 9.12208 3.46013 8.90729 3.71698C8.66405 4.00784 8.53292 4.40125 8.27064 5.18807L8 6" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>""",
+                            width="24",
+                            height="24",
+                            viewBox="0 0 24 24",
+                            fill="none",
+                            cls="htmx-indicator w-8 h-8 fill-red-300 hover:fill-red-100",
+                        ),
+                        hx_delete=f"/key/{k.id}",
+                        hx_indicator="#spinner",
+                        hx_target="closest div",
+                        hx_swap="outerHTML swap:1s",
+                        hx_confirm="Are you sure?",
+                        cls="w-1/8",
+                    ),
+                    fh.Style(
+                        """
+                        div.htmx-swapping {
+                            opacity: 0;
+                            transition: opacity 1s ease-out;
+                        }
+                        """
+                    ),
                     fh.Div(
                         obscured_key,
                         onmouseover=(
@@ -387,13 +394,13 @@ def modal_get():  # noqa: C901
                         hx_indicator="#spinner",
                         hx_target="#toast-container",
                         hx_swap="outerHTML",
-                        cls="text-blue-300 hover:text-blue-100 cursor-pointer w-2/3",
+                        cls="text-blue-300 hover:text-blue-100 cursor-pointer w-5/8",
                         title="Click to copy",
                         id=f"key-element-{k.id}",
                     ),
                     fh.Div(
                         k.granted_at.strftime("%Y-%m-%d %H:%M:%S"),
-                        cls="w-1/3",
+                        cls="w-3/8",
                     ),
                     id=f"key-{k.id}",
                     cls="flex p-2",
@@ -593,39 +600,37 @@ def modal_get():  # noqa: C901
                 cls="text-xl text-blue-300 hover:text-blue-100 font-mono font-family:Consolas, Monaco, 'Lucida Console', 'Liberation Mono', 'DejaVu Sans Mono', 'Bitstream Vera Sans Mono', 'Courier New'",
             ),
             fh.Svg(
-                fh.NotStr(
-                    """<style>
-                        .spinner_zWVm { animation: spinner_5QiW 1.2s linear infinite, spinner_PnZo 1.2s linear infinite; }
-                        .spinner_gfyD { animation: spinner_5QiW 1.2s linear infinite, spinner_4j7o 1.2s linear infinite; animation-delay: .1s; }
-                        .spinner_T5JJ { animation: spinner_5QiW 1.2s linear infinite, spinner_fLK4 1.2s linear infinite; animation-delay: .1s; }
-                        .spinner_E3Wz { animation: spinner_5QiW 1.2s linear infinite, spinner_tDji 1.2s linear infinite; animation-delay: .2s; }
-                        .spinner_g2vs { animation: spinner_5QiW 1.2s linear infinite, spinner_CMiT 1.2s linear infinite; animation-delay: .2s; }
-                        .spinner_ctYB { animation: spinner_5QiW 1.2s linear infinite, spinner_cHKR 1.2s linear infinite; animation-delay: .2s; }
-                        .spinner_BDNj { animation: spinner_5QiW 1.2s linear infinite, spinner_Re6e 1.2s linear infinite; animation-delay: .3s; }
-                        .spinner_rCw3 { animation: spinner_5QiW 1.2s linear infinite, spinner_EJmJ 1.2s linear infinite; animation-delay: .3s; }
-                        .spinner_Rszm { animation: spinner_5QiW 1.2s linear infinite, spinner_YJOP 1.2s linear infinite; animation-delay: .4s; }
-                        @keyframes spinner_5QiW { 0%, 50% { width: 7.33px; height: 7.33px; } 25% { width: 1.33px; height: 1.33px; } }
-                        @keyframes spinner_PnZo { 0%, 50% { x: 1px; y: 1px; } 25% { x: 4px; y: 4px; } }
-                        @keyframes spinner_4j7o { 0%, 50% { x: 8.33px; y: 1px; } 25% { x: 11.33px; y: 4px; } }
-                        @keyframes spinner_fLK4 { 0%, 50% { x: 1px; y: 8.33px; } 25% { x: 4px; y: 11.33px; } }
-                        @keyframes spinner_tDji { 0%, 50% { x: 15.66px; y: 1px; } 25% { x: 18.66px; y: 4px; } }
-                        @keyframes spinner_CMiT { 0%, 50% { x: 8.33px; y: 8.33px; } 25% { x: 11.33px; y: 11.33px; } }
-                        @keyframes spinner_cHKR { 0%, 50% { x: 1px; y: 15.66px; } 25% { x: 4px; y: 18.66px; } }
-                        @keyframes spinner_Re6e { 0%, 50% { x: 15.66px; y: 8.33px; } 25% { x: 18.66px; y: 11.33px; } }
-                        @keyframes spinner_EJmJ { 0%, 50% { x: 8.33px; y: 15.66px; } 25% { x: 11.33px; y: 18.66px; } }
-                        @keyframes spinner_YJOP { 0%, 50% { x: 15.66px; y: 15.66px; } 25% { x: 18.66px; y: 18.66px; } }
-                    </style>
-                    <rect class="spinner_zWVm" x="1" y="1" width="7.33" height="7.33"/>
-                    <rect class="spinner_gfyD" x="8.33" y="1" width="7.33" height="7.33"/>
-                    <rect class="spinner_T5JJ" x="1" y="8.33" width="7.33" height="7.33"/>
-                    <rect class="spinner_E3Wz" x="15.66" y="1" width="7.33" height="7.33"/>
-                    <rect class="spinner_g2vs" x="8.33" y="8.33" width="7.33" height="7.33"/>
-                    <rect class="spinner_ctYB" x="1" y="15.66" width="7.33" height="7.33"/>
-                    <rect class="spinner_BDNj" x="15.66" y="8.33" width="7.33" height="7.33"/>
-                    <rect class="spinner_rCw3" x="8.33" y="15.66" width="7.33" height="7.33"/>
-                    <rect class="spinner_Rszm" x="15.66" y="15.66" width="7.33" height="7.33"/>
-                    """
-                ),
+                """<style>
+                    .spinner_zWVm { animation: spinner_5QiW 1.2s linear infinite, spinner_PnZo 1.2s linear infinite; }
+                    .spinner_gfyD { animation: spinner_5QiW 1.2s linear infinite, spinner_4j7o 1.2s linear infinite; animation-delay: .1s; }
+                    .spinner_T5JJ { animation: spinner_5QiW 1.2s linear infinite, spinner_fLK4 1.2s linear infinite; animation-delay: .1s; }
+                    .spinner_E3Wz { animation: spinner_5QiW 1.2s linear infinite, spinner_tDji 1.2s linear infinite; animation-delay: .2s; }
+                    .spinner_g2vs { animation: spinner_5QiW 1.2s linear infinite, spinner_CMiT 1.2s linear infinite; animation-delay: .2s; }
+                    .spinner_ctYB { animation: spinner_5QiW 1.2s linear infinite, spinner_cHKR 1.2s linear infinite; animation-delay: .2s; }
+                    .spinner_BDNj { animation: spinner_5QiW 1.2s linear infinite, spinner_Re6e 1.2s linear infinite; animation-delay: .3s; }
+                    .spinner_rCw3 { animation: spinner_5QiW 1.2s linear infinite, spinner_EJmJ 1.2s linear infinite; animation-delay: .3s; }
+                    .spinner_Rszm { animation: spinner_5QiW 1.2s linear infinite, spinner_YJOP 1.2s linear infinite; animation-delay: .4s; }
+                    @keyframes spinner_5QiW { 0%, 50% { width: 7.33px; height: 7.33px; } 25% { width: 1.33px; height: 1.33px; } }
+                    @keyframes spinner_PnZo { 0%, 50% { x: 1px; y: 1px; } 25% { x: 4px; y: 4px; } }
+                    @keyframes spinner_4j7o { 0%, 50% { x: 8.33px; y: 1px; } 25% { x: 11.33px; y: 4px; } }
+                    @keyframes spinner_fLK4 { 0%, 50% { x: 1px; y: 8.33px; } 25% { x: 4px; y: 11.33px; } }
+                    @keyframes spinner_tDji { 0%, 50% { x: 15.66px; y: 1px; } 25% { x: 18.66px; y: 4px; } }
+                    @keyframes spinner_CMiT { 0%, 50% { x: 8.33px; y: 8.33px; } 25% { x: 11.33px; y: 11.33px; } }
+                    @keyframes spinner_cHKR { 0%, 50% { x: 1px; y: 15.66px; } 25% { x: 4px; y: 18.66px; } }
+                    @keyframes spinner_Re6e { 0%, 50% { x: 15.66px; y: 8.33px; } 25% { x: 18.66px; y: 11.33px; } }
+                    @keyframes spinner_EJmJ { 0%, 50% { x: 8.33px; y: 15.66px; } 25% { x: 11.33px; y: 18.66px; } }
+                    @keyframes spinner_YJOP { 0%, 50% { x: 15.66px; y: 15.66px; } 25% { x: 18.66px; y: 18.66px; } }
+                </style>
+                <rect class="spinner_zWVm" x="1" y="1" width="7.33" height="7.33"/>
+                <rect class="spinner_gfyD" x="8.33" y="1" width="7.33" height="7.33"/>
+                <rect class="spinner_T5JJ" x="1" y="8.33" width="7.33" height="7.33"/>
+                <rect class="spinner_E3Wz" x="15.66" y="1" width="7.33" height="7.33"/>
+                <rect class="spinner_g2vs" x="8.33" y="8.33" width="7.33" height="7.33"/>
+                <rect class="spinner_ctYB" x="1" y="15.66" width="7.33" height="7.33"/>
+                <rect class="spinner_BDNj" x="15.66" y="8.33" width="7.33" height="7.33"/>
+                <rect class="spinner_rCw3" x="8.33" y="15.66" width="7.33" height="7.33"/>
+                <rect class="spinner_Rszm" x="15.66" y="15.66" width="7.33" height="7.33"/>
+                """,
                 width="24",
                 height="24",
                 viewBox="0 0 24 24",
@@ -641,12 +646,26 @@ def modal_get():  # noqa: C901
                 ),
                 fh.Div(
                     fh.A(
-                        icon(si_github.svg),
+                        fh.Svg(
+                            si_github.svg,
+                            width="35",
+                            height="35",
+                            viewBox="0 0 15 15",
+                            fill="none",
+                            cls="rounded p-0.5 hover:bg-zinc-700 cursor-pointer",
+                        ),
                         href="https://github.com/andrewhinh/formless",
                         target="_blank",
                     ),
                     fh.A(
-                        icon(si_pypi.svg),
+                        fh.Svg(
+                            si_pypi.svg,
+                            width="35",
+                            height="35",
+                            viewBox="0 0 15 15",
+                            fill="none",
+                            cls="rounded p-0.5 hover:bg-zinc-700 cursor-pointer",
+                        ),
                         href="https://pypi.org/project/formless/",
                         target="_blank",
                     ),
@@ -1316,6 +1335,31 @@ def modal_get():  # noqa: C901
                 db_session.commit()
         fh.add_toast(session, "Deleted keys.", "success")
         return fh.RedirectResponse("/developer", status_code=303)
+
+    @f_app.delete("/key/{key_id}")
+    def delete_key(
+        session,
+        key_id: int,
+    ):
+        with get_db_session() as db_session:
+            key = db_session.get(ApiKey, key_id)
+            db_session.delete(key)
+            db_session.commit()
+        fh.add_toast(session, "Deleted key.", "success")
+        keys_present = bool(get_curr_keys(session["session_id"], number=1))
+        return (
+            "",
+            num_keys(len(get_curr_keys(session["session_id"])), "true"),
+            key_manage(
+                keys_present,
+                "true",
+            ),
+            key_load_more(
+                keys_present,
+                False,
+                hx_swap_oob="true",
+            ),
+        )
 
     ## export to CSV
     @f_app.get("/export-gens")
